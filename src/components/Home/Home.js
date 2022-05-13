89531be3ae1eb591757356f51336dab79a8a0e0b
max <maxime.jalbert.locke@gmail.com>
2022-05-13 14:12:23 +1000

    Neater Names

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,9 @@
-import tt_display from '../Timetable/tt_display';
+import Timetable from '../Timetable/tt_display';
 
 function Home() {
     return (
         <div>
-            <tt_display />
+            <Timetable />
         </div>
     );
 }
